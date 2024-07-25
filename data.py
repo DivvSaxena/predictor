0ce7782335743da7b95557e0a45e1d2d965c1cfd
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2024-07-25 14:37:54 +0530

    targetting only first five columns

--- data.py
+++ data.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 url = r"C:/Users/Lenovo/Desktop/predictor/monatszahlen2405_verkehrsunfaelle_export_31_05_24_r.csv"
 data = pd.read_csv(url)
 
+# Assuming your column names are 'Category', 'Accident-type', 'JAHR', 'MONAT', and 'WERT'
+selected_columns = ['MONATSZAHL', 'AUSPRAEGUNG', 'JAHR', 'MONAT', 'WERT']  # Adjust column names if needed
+
+# Select the first 5 columns
+data = data[selected_columns]
+
 # Display the first few rows of the dataset
 data.head()
 
