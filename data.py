45f85acf98e1503d2f93dc83484e05d366bb2e5c
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2024-07-25 14:25:00 +0530

    displaying the first few rows after filtering

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the dataset
-url = r'C:\Users\Lenovo\Desktop\ai-model\monatszahlen2405_verkehrsunfaelle_export_31_05_24_r.csv'
+url = r"C:/Users/Lenovo/Desktop/predictor/monatszahlen2405_verkehrsunfaelle_export_31_05_24_r.csv"
 data = pd.read_csv(url)
 
 # Display the first few rows of the dataset
@@ -13,4 +13,6 @@ data = data[data['JAHR'] <= 2020]
 
 # Display the first few rows after filtering
 data.head()
+
+print(data.head())
 
